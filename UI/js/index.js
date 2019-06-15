593a90a3bd5121dba1b3946e991d832c06b0cef1
chubi best <gokayirayol@gmail.com>
2019-06-15 18:24:51 +0100

    feat(ui): See intervention location on google maps [Starts #166625685]

--- UI/js/index.js
+++ UI/js/index.js
@@ -5,19 +5,19 @@ const nav = document.querySelector('#nav_links')
 const links = document.querySelectorAll('#nav_links li');
 const menu = document.querySelector('#menu')
 
-const handleMenu = (e) => {
-  e.preventDefault()
-menu.style.display = 'none'
-links.forEach(link => {
-  nav.addEventListener('mouseout', (e) => {
-    e.preventDefault();
-    menu.style.display = 'inline'
-    link.style.display = 'none'
-  });
-  link.style.display = 'block'
-})
+// const handleMenu = (e) => {
+//   e.preventDefault()
+// menu.style.display = 'none'
+// links.forEach(link => {
+//   nav.addEventListener('mouseout', (e) => {
+//     e.preventDefault();
+//     menu.style.display = 'inline'
+//     link.style.display = 'none'
+//   });
+//   link.style.display = 'block'
+// })
 
-}
+// }
 // menu.addEventListener('mouseover', (e) => {handleMenu(e)})
 const toggleDisplay = (firstAnimation, secondAnimation) => {
   firstAnimation.classList.toggle('display')
