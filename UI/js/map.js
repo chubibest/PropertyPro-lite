5752773532c50f200257357755d2a934ff4c5110
chubi best <gokayirayol@gmail.com>
2019-06-17 00:48:25 +0100

    feat(ui): View specific property type [Starts ##166625581]

---
+++ UI/js/map.js
@@ -0,0 +1,24 @@
+  function initMap () {
+    const mapDiv =  document.getElementById('map');
+    mapDiv.style.display = "block"
+  
+    const latLong = {
+      lat: -25.344,
+      lng: 131.036
+    }
+    const mapOptions = {
+      zoom: 8,
+      center: latLong
+    }
+    const map = new google.maps.Map(mapDiv, mapOptions);
+
+    const markerOptions = {
+      position: latLong,
+      map,
+      title: 'Property location.'
+    }
+    const marker = new google.maps.Marker(markerOptions)
+
+    mapDiv.insertAdjacentElement('beforebegin',document.getElementById('close'))
+   }
+  
