02b9e8aced3214daff4f60286c00a5bf899bbeb7
chubi best <gokayirayol@gmail.com>
2019-06-19 09:14:25 +0100

    feat(ui): Delete property ad [Finishes #166625560][starts #166625519] [Finishes #166625519][starts #166625491] [Finishes #166625491]

--- UI/js/index.js
+++ UI/js/index.js
@@ -4,20 +4,6 @@ const secondAnimation = document.querySelector('#second_animation');
 const nav = document.querySelector('#nav_links')
 
 
-// const handleMenu = (e) => {
-//   e.preventDefault()
-// menu.style.display = 'none'
-// links.forEach(link => {
-//   nav.addEventListener('mouseout', (e) => {
-//     e.preventDefault();
-//     menu.style.display = 'inline'
-//     link.style.display = 'none'
-//   });
-//   link.style.display = 'block'
-// })
-
-// }
-// menu.addEventListener('mouseover', (e) => {handleMenu(e)})
 const toggleDisplay = (firstAnimation, secondAnimation) => {
   firstAnimation.classList.toggle('reel')
   secondAnimation.classList.toggle('reel')
