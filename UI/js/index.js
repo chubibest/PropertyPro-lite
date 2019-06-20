4e2942c4faca77228fb8628748955f7638e84208
chubi best <gokayirayol@gmail.com>
2019-06-20 21:08:20 +0100

    chore(ui): Setup backend environment [Finishes #166824188][Finishes #166816922]

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,14 +1,12 @@
 //  acquire animation divs from dom
 const firstAnimation = document.querySelector('#first_animation');
 const secondAnimation = document.querySelector('#second_animation');
-const nav = document.querySelector('#nav_links')
 
+const toggleDisplay = () => {
+  firstAnimation.classList.toggle('reel');
+  secondAnimation.classList.toggle('reel');
+};
 
-const toggleDisplay = (firstAnimation, secondAnimation) => {
-  firstAnimation.classList.toggle('reel')
-  secondAnimation.classList.toggle('reel')
-}
-
-setInterval(()=>{
-  toggleDisplay(firstAnimation, secondAnimation)
-}, 10000)
+setInterval(() => {
+  toggleDisplay(firstAnimation, secondAnimation);
+}, 10000);
