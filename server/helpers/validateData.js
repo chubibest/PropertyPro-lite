aff0c0b61cf30a8cb1a1f75df93474717fc317f9
chubi best <gokayirayol@gmail.com>
2019-06-25 19:18:55 +0100

    bg(api): Fix joi error handling [Finishes #166910457]

--- server/helpers/validateData.js
+++ server/helpers/validateData.js
@@ -1,17 +1,17 @@
 import Joi from '@hapi/joi';
-import errorResponse from '../controllers/response';
 
-
-const signUpSchema = {
-  username: Joi.string().alphanum().min(3).max(30)
+const signUpSchema = Joi.object().keys({
+  username: Joi.string().trim().alphanum().min(3)
+    .max(30)
     .required(),
-  password: Joi.string().regex(/.{3,30}/).required(),
-  firstname: Joi.string().required(),
-  lastname: Joi.string().required(),
+  password: Joi.string().trim().regex(/.{3,30}/).required(),
+  firstname: Joi.string().trim().required(),
+  lastname: Joi.string().trim().strict(),
   phonenumber: Joi.number().required(),
-  address: Joi.string().required(),
-  email: Joi.string().email({ minDomainSegments: 2 }).required()
-};
+  address: Joi.string().trim().required(),
+  email: Joi.string().trim().email({ minDomainSegments: 2 }).required(),
+  isAdmin: Joi.string().trim().required()
+});
 
 const signInSchema = {
   username: Joi.string().alphanum().min(3).max(30)
@@ -19,17 +19,26 @@ const signInSchema = {
   password: Joi.string().regex(/.{3,30}/).required()
 };
 
-export const validateSignup = async (req, res, next) => {
-  Joi.validate(req.body, signUpSchema, (error) => {
-    if (error) {
-      return errorResponse(res, error, 400);
-    }
+export const validateSignup = async ({ body }, res, next) => {
+  try {
+    await Joi.validate(body, signUpSchema);
     next();
-  });
+  } catch (e) {
+    res.status(400).send({
+      status: 'error',
+      error: e.details[0].message
+    });
+  }
 };
 
-export const validateSignin = async (req, res, next) => {
-  Joi.validate(req.body, signInSchema)
-    .then(() => next())
-    .catch(error => errorResponse(res, error, 400));
+export const validateSignin = async ({ body }, res, next) => {
+  try {
+    await Joi.validate(body, signInSchema);
+    next();
+  } catch (e) {
+    res.status(400).send({
+      status: 'error',
+      error: e.details[0].message
+    });
+  }
 };
