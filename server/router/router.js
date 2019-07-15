b1dff7207c18ccf3252bb18565e340356b0b8bbe
chubi best <gokayirayol@gmail.com>
2019-07-15 13:33:06 +0100

    chore: Rename routes [Finishes #167289371]

--- server/router/router.js
+++ server/router/router.js
@@ -14,15 +14,15 @@ dotenv.config();
 
 const router = new Router();
 
-router.post('/api/v1/auth/signup', validateSignup, createUsers);
-router.post('/api/v1/auth/signin', validateSignin, login);
-router.post('/api/v1/property', validatePostAd, auth, createAds);
-router.patch('/api/v1/property/:property_id', validateUpdateAd, auth, updateAd);
-router.patch('/api/v1/property/:property_id/sold', auth, changeStatus);
-router.delete('/api/v1/property/:property_id', auth, deletePropertyAd);
-router.get('/api/v1/property', auth, getByType, getAllAds);
-router.get('/api/v1/property/:property_id', auth, getAdById);
+router.post('/auth/signup', validateSignup, createUsers);
+router.post('/auth/signin', validateSignin, login);
+router.post('/property', validatePostAd, auth, createAds);
+router.patch('/property/:property_id', validateUpdateAd, auth, updateAd);
+router.patch('/property/:property_id/sold', auth, changeStatus);
+router.delete('/property/:property_id', auth, deletePropertyAd);
+router.get('/property', auth, getByType, getAllAds);
+router.get('/property/:property_id', auth, getAdById);
 router.post('/auth/:user_email/reset_password', resetpasscontroller, validatePassChange, auth, changePass);
-router.post('/api/v1/property/:property_id', validateFlag, auth, flag);
+router.post('/property/:property_id', validateFlag, auth, flag);
 
 export default router;
